3959b04867a01991499a7793fdd7379c39b47095
hlomla <hlomla.tapuko@gmail.com>
2022-06-24 16:24:53 +0200

    cant access app

--- client/login.js
+++ client/login.js
@@ -1,27 +1,28 @@
 import axios from 'axios';
 import Register from './register';
 
-export default function Login () {
+export default function Login() {
 
     return {
         ...Register(),
-        login_message:'',
+        login_message: '',
         open: false,
 
         checkToken() {
             axios
-            .post(`/api/login`, this.registerUser())
-            .then(r => this.registerUser())
+                .post(`/api/login`, this.registerUser())
+                .then(r => this.registerUser())
             alert('loggeddd')
-            .then(() => {
-                console.log(userData);
-                this.registered = userData.data
-                this.login_message = 'Logged In!!!';
-                this.open = true;
-            })
-            
-            .catch(err => console.log(err))
+                .then(() => {
+                    console.log(userData);
+                    this.registered = userData.data
+                    this.login_message = 'Logged In!!!';
+                    this.open = true;
+                })
+
+                .catch(err => console.log(err))
         },
+
 
     }
 }
